b1b57fedae40fec94ba2ece14eadc153763d4f85
NeehalSaleh <nssalmuways@sm.imamu.edu.sa>
2022-06-19 00:01:04 +0300

    Enter

---
+++ block.js
@@ -0,0 +1,30 @@
+import sha256 from 'crypto-js/sha256.js';
+class block {
+
+
+    constructor( hight,timestamp, data, prevHash='') {
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = this.calchash();
+    this.prevHash = prevHash; 
+    this.hight=hight;
+    this.nonce=0;
+    this.numberOfGuesses = 0; 
+
+}
+
+calchash(){
+    return  sha256(this.hight+ this.prevHash+ this.timestamp+ JSON.stringify(this.data)+this.nonce).toString();
+}
+mine(difficult) {
+
+
+    while (this.hash.substring(0, difficult)!=="".padStart(difficult, "0")) {
+        this.nonce++;
+     this.numberOfGuesses++;
+        this.hash = this.calchash();
+    }
+    console.log("block mined:" + this.hash+"" +this.numberOfGuesses);
+ }
+}
+export default block;
--- blockchain.js
+++ blockchain.js
@@ -1,33 +1,4 @@
-import sha256 from 'crypto-js/sha256.js';
-class block {
-
-
-    constructor( hight,timestamp, data, prevHash='') {
-    this.timestamp = timestamp;
-    this.data = data;
-    this.hash = this.calchash();
-    this.prevHash = prevHash; 
-    this.hight=hight;
-    this.nonce=0;
-    this.numberOfGuesses = 0; 
-
-}
-
-calchash(){
-    return  sha256(this.hight+ this.prevHash+ this.timestamp+ JSON.stringify(this.data)+this.nonce).toString();
-}
-mine(difficult) {
-
-
-    while (this.hash.substring(0, difficult)!=="".padStart(difficult, "0")) {
-        this.nonce++;
-     this.numberOfGuesses++;
-        this.hash = this.calchash();
-    }
-    console.log("block mined:" + this.hash);
- }
-}
-
+import block from './block.js'
 class blockchain{
 
 constructor() {
@@ -35,7 +6,7 @@ constructor() {
     this.chain = [this.genesis()];
     this.difficult=4;
 }
-genesis(){
+genesis(){ 
     return new block(0,"01/15/2022","this is the first block","0");
 }
 
@@ -72,10 +43,35 @@ isValid(blockchain = this) {
 
 }
 
-
 let neehal = new blockchain();
 console.log('mining block 1:');
-neehal.addBlock(new block(1, "1/6/8888", { amount :4}));
+neehal.addBlock(new block(1, "16/06/2022", { amount :4}));
+
 console.log('mining block 2:');
-neehal.addBlock(new block(2, "1/6/8888", {amoun :60}));
-console.log();
+neehal.addBlock(new block(2, "17/06/2022", {amoun :60}));
+
+console.log('mining block 3:');
+neehal.addBlock(new block(1, "18/06/2022", { amount :90}));
+
+console.log('mining block 4:');
+neehal.addBlock(new block(2, "19/06/2022", {amoun :10000}));
+
+console.log('mining block 5:');
+neehal.addBlock(new block(1, "20/06/2022", { amount :200}));
+
+console.log('mining block 6:');
+neehal.addBlock(new block(2, "21/06/2022", {amoun :8900}));
+
+console.log('mining block 7:');
+neehal.addBlock(new block(1, "22/06/2022", { amount :9820}));
+
+console.log('mining block 8:');
+neehal.addBlock(new block(2, "23/06/2022", {amoun :900}));
+
+console.log('mining block 9:');
+neehal.addBlock(new block(1, "24/06/2022", { amount :8887}));
+
+console.log('mining block 10:');
+neehal.addBlock(new block(2, "25/06/2022", {amoun :8908}));
+console.log('is our neehal Blockchain valid or not?'+ neehal.isValid());
+console.log(neehal.genesis());
